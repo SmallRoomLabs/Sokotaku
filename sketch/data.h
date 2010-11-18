644ab65835dd5c38cf8d15a854cd018edc3f9d85
Small Room Labs <git@smallroomlabs.com>
2010-11-19 00:36:57 +0800

    Removed old file left over from the previous move

--- sketch/data.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DATA_H_
-#define _DATA_H_
-
-extern unsigned char ASCII_Char[52][8];
-extern unsigned char ASCII_Number[10][8];
-extern unsigned char ASCII_Space[8];
-extern unsigned char SmallDigits[25];
-
-#endif
