ab525f13c0465d7600a9d3db6a09adac706d3ece
Small Room Labs <git@smallroomlabs.com>
2010-11-19 00:33:52 +0800

    The files for the Arduino sketch moved to sketch/Sokotaku

---
+++ sketch/Sokotaku/data.h
@@ -0,0 +1,9 @@
+#ifndef _DATA_H_
+#define _DATA_H_
+
+extern unsigned char ASCII_Char[52][8];
+extern unsigned char ASCII_Number[10][8];
+extern unsigned char ASCII_Space[8];
+extern unsigned char SmallDigits[25];
+
+#endif
